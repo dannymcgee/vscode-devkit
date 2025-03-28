adbedf9ccadf81b8ed2f0123a1bbe5361d208049
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-03-29 00:12:20 +0100

    Add `minify` and `sourceMaps` options to `build` (#2)
    - Add `minify` and `sourceMaps` options to `build`
    - Add `production` and `development` configurations to the `extension`
      generator
    
    Allows debugging the extensions in VS Code.
    
    ---------
    
    Co-authored-by: Danny McGee <dannymcgee@gmail.com>

--- packages/nx/src/executors/build/schema.d.ts
+++ packages/nx/src/executors/build/schema.d.ts
@@ -1,11 +1,13 @@
 import { AssetGlob } from "@nx/js/src/utils/assets/assets";
 
 export default interface Options {
-	assets?: (string|AssetGlob)[];
+	assets?: (string | AssetGlob)[];
 	additionalTargets?: string[];
 	entryPoint: string;
 	outputPath: string;
 	outputFile: string;
 	package?: boolean;
 	install?: boolean;
+	minify?: boolean;
+	sourceMaps?: boolean;
 }
